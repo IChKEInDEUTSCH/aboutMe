f12862255353427e13b8f32ba67fa3d0ddb27ea6
ichKeinDeustch <102silencer@gmail.com>
2025-07-16 14:35:37 +0800

    Simplify the router view

--- src/lavaLamp.vue
+++ src/lavaLamp.vue
@@ -580,7 +580,7 @@ class OptimizedLavaLamp {
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   z-index: 0;
   pointer-events: none;
@@ -590,7 +590,7 @@ class OptimizedLavaLamp {
   position: absolute;
   top: 0;
   left: 0;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   display: block;
   z-index: 0;
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
+
   {
     path: '/',
-    component: () => import('@/layouts/MainLayout.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        component: () => import('@/views/HomeView.vue')
-      },
-      {
-        path: 'info',
-        name: 'Info',
-        component: () => import('@/pages/info.vue')
-      },
-      {
-        path: 'experience',
-        name: 'Experience',
-        component: () => import('@/pages/experience.vue')
-      },
-      {
-        path: 'contest',
-        name: 'Contest',
-        component: () => import('@/pages/contest.vue')
-      },
-      {
-        path: 'portfolio',
-        name: 'Portfolio',
-        component: () => import('@/pages/portfolio.vue')
-      }
-    ]
+    name: 'Home',
+    component: () => import('@/pages/HomeView.vue')
+  },
+  {
+    path: '/info',
+    name: 'Info',
+    component: () => import('@/pages/info.vue')
+  },
+  {
+    path: '/experience',
+    name: 'Experience',
+    component: () => import('@/pages/experience.vue')
+  },
+  {
+    path: '/contest',
+    name: 'Contest',
+    component: () => import('@/pages/contest.vue')
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: () => import('@/pages/portfolio.vue')
   },
   {
     // Catch-all route for 404 pages
